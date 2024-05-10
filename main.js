77dd8efb85acd6658eb73b6fc0cd6a81cd769662
BryanS1989 <bryanstevenal@gmail.om>
2024-05-10 16:03:47 +0200

    Event handling

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data () {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             description: 'Vintage green socks',
             inStock: true,
-            inventory: 100,
+            inventory: 15,
             details: [
                 '50% cotton',
                 '30% wool',
@@ -15,12 +16,27 @@ const app = Vue.createApp({
                 {
                     id: 2234,
                     color: 'green',
+                    image: './assets/images/socks_green.jpg',
                 },
                 {
                     id: 2235,
                     color: 'blue',
+                    image: './assets/images/socks_blue.jpg',
                 },
             ]
+        }
+    },
+    methods: {
+        addToCart () {
+            if (this.cart === this.inventory) return;
+            this.cart += 1;
+        },
+        remove1FromCart () {
+            if (this.cart === 0) return;
+            this.cart -= 1;
+        },
+        updateImage (variantImage) {
+            this.image = variantImage;
         }
     }
 });
